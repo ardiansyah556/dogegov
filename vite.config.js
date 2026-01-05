51d7835af0f993039e9bc4e3f96c7dbd5c55e6d4
yezzky8 <yezzky8@gmail.com>
2026-01-05 07:38:50 +0000

    fix: overhaul build configuration and resolve dependencies

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,13 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: 'client',
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './client/src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
-    outDir: '../dist'
+    outDir: 'dist'
   }
 })
